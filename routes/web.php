7582725291d201dbc326596c626f1e8ade09b97c
ChristopherKSmith <k_smith_26@hotmail.com>
2017-07-15 12:50:43 -0400

    users edit functionality created

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,16 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::get('/admin', function(){
+
+
+
+
+
+Route::group(['middleware' => 'admin'], function(){
+	Route::get('/admin', function(){
 	return view('admin.index');
+	});
+
+	Route::resource('admin/users', 'AdminUsersController');
+	
 });
-
-
-Route::resource('admin/users', 'AdminUsersController');
